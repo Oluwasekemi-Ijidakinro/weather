36218bea0ab01dd6375fa6a6f9205fa68a749368
Oluwasekemi-Ijidakinro <ijidakinrooluwasekemi@gmail.com>
2022-08-04 20:56:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let weather = {
+    // "apikey": "efe081eb7aed5984c0d4b6cdb065783d"
+    "apikey": "e2c989ab7c6f71181a70e14d81cb2a7f"
+
+}
+let url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
